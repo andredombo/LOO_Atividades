a65f0165c1b10484644fec9d0615bc2cc1e84223
André Abuchaim <andreabuchaim.as@gmail.com>
2023-09-03 23:24:34 -0400

    first commit

---
+++ Atividade_4/Questao_1/ProjetoEstagiario/Funcionario.java
@@ -0,0 +1,23 @@
+public class Funcionario extends BasePessoa {
+    private String contaCorrente;
+    private String cracha;
+    public String getContaCorrente() {
+        return contaCorrente;
+    }
+    public void setContaCorrente(String contaCorrente) {
+        this.contaCorrente = contaCorrente;
+    }
+    public String getCracha() {
+        return cracha;
+    }
+    public void setCracha(String cracha) {
+        this.cracha = cracha;
+    }
+    public Funcionario(String contaCorrente, String cracha) {
+        this.contaCorrente = contaCorrente;
+        this.cracha = cracha;
+    }
+    public Funcionario(){
+        
+    }
+}
---
+++ Atividade_4/Questao_1/ProjetoEstagiario/Passageiro.java
@@ -0,0 +1,23 @@
+public class Passageiro extends BasePessoa{
+    private String numeroCartao;
+    private String documento;
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+    public void setNumeroCartao(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+    public String getDocumento() {
+        return documento;
+    }
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+    public Passageiro(String numeroCartao, String documento) {
+        this.numeroCartao = numeroCartao;
+        this.documento = documento;
+    }
+    public Passageiro(){
+        
+    }
+}
